7ca65d55c6fc3ea6b2ae72617728133ec392bc6a
Chris Warner <cwarner818@gmail.com>
2018-02-03 18:51:31 -0700

    Formatting fix

--- metrics.go
+++ metrics.go
@@ -24,8 +24,9 @@ SOFTWARE.
 package spamalot
 
 import (
-	"time"
 	"log"
+	"time"
+
 	"github.com/CWarner818/giota"
 )
 
@@ -162,10 +163,10 @@ func (mr *metricsrouter) printMetrics(txAndNode txandnode) {
 	// send current state of the spammer
 	if mr.relay != nil {
 		summary := Summary{
-			TXsSucceeded:   mr.txsSucceeded, TXsFailed: mr.txsFailed,
-			BadBranch:      mr.badBranch, BadTrunk: mr.badBranch, BadTrunkAndBranch: mr.badTrunkAndBranch,
+			TXsSucceeded: mr.txsSucceeded, TXsFailed: mr.txsFailed,
+			BadBranch: mr.badBranch, BadTrunk: mr.badBranch, BadTrunkAndBranch: mr.badTrunkAndBranch,
 			MilestoneTrunk: mr.milestoneTrunk, MilestoneBranch: mr.milestoneBranch,
-			TPS:            tps, ErrorRate: 100 - successRate,
+			TPS: tps, ErrorRate: 100 - successRate,
 		}
 		mr.relay <- Metric{Kind: SUMMARY, Data: summary}
 
